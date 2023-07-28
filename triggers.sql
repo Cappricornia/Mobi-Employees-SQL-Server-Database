fda2266f1e7f98ac2f85e42936b48e74f34c165a
Diyana Hristova <90700181+Cappricornia@users.noreply.github.com>
2023-07-28 10:51:36 +0300

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -1,11 +1,11 @@
 -- MOBI DB
 
--- Create Table Salary Logs and add trigger to it each time the salary is updated.
+-- Create Table Salary Logs and add a trigger to it each time the salary is updated.
 
 CREATE TABLE SalaryLogs(
 	[Id] INT PRIMARY KEY IDENTITY NOT NULL,
 	EmployeeID INT NOT NULL,
-    OldSalary MONEY,
+        OldSalary MONEY,
 	NewSalary MONEY,
 	UpdateDate DATETIME2
 )
@@ -15,14 +15,16 @@ CREATE TRIGGER tr_SalaryLogs
 ON Employees FOR UPDATE
 AS
 INSERT INTO SalaryLogs(EmployeeID, OldSalary, NewSalary, UpdateDate)
-SELECT d.EmployeeID, d.Salary, i.Salary, GETDATE() FROM inserted AS i
-JOIN deleted AS d 
-ON i.EmployeeID = d.EmployeeID AND i.Salary <> d.Salary
+SELECT d.EmployeeID, d.Salary, i.Salary, GETDATE() 
+	FROM inserted AS i
+	JOIN deleted AS d 
+    ON i.EmployeeID = d.EmployeeID AND i.Salary <> d.Salary
 
 
 -- Update 
 
-UPDATE Employees SET Salary += 1500 WHERE EmployeeId IN (1, 4, 6, 9);
+UPDATE Employees SET Salary += 1500 
+	WHERE EmployeeId IN (1, 4, 6, 9);
 
 -- Select
 SELECT * FROM SalaryLogs
